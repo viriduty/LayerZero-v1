a943fdac4fdb9e57c97dc753c6a189badd48c6ec
viriduty <dsffdsdsfsdfdsrgffsddfsdfsdsf@gmail.com>
2024-12-15 14:09:12 +0000

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,35 +1,38 @@
 module.exports = {
     overrides: [
         {
+            // Solidity files
             files: "*.sol",
             options: {
-                bracketSpacing: false,
-                printWidth: 300,
-                tabWidth: 4,
-                useTabs: false,
-                singleQuote: false,
-                explicitTypes: "never",
+                bracketSpacing: false,   // No spaces between brackets
+                printWidth: 300,         // Maximum line length
+                tabWidth: 4,             // 4 spaces for indentation
+                useTabs: false,          // Use spaces instead of tabs
+                singleQuote: false,      // Use double quotes
+                explicitTypes: "never",  // Do not explicitly type function parameters
             },
         },
         {
+            // TypeScript files
             files: "*.ts",
             options: {
-                printWidth: 145,
-                semi: false,
-                tabWidth: 4,
-                useTabs: false,
-                trailingComma: "es5",
+                printWidth: 145,         // Maximum line length
+                semi: false,             // No semicolons at the end of statements
+                tabWidth: 4,             // 4 spaces for indentation
+                useTabs: false,          // Use spaces instead of tabs
+                trailingComma: "es5",    // Trailing commas where valid in ES5 (e.g., in objects/arrays)
             },
         },
         {
+            // JavaScript files
             files: "*.js",
             options: {
-                printWidth: 145,
-                semi: false,
-                tabWidth: 4,
-                useTabs: false,
-                trailingComma: "es5",
+                printWidth: 145,         // Maximum line length
+                semi: false,             // No semicolons at the end of statements
+                tabWidth: 4,             // 4 spaces for indentation
+                useTabs: false,          // Use spaces instead of tabs
+                trailingComma: "es5",    // Trailing commas where valid in ES5
             },
         },
     ],
-}
+};
